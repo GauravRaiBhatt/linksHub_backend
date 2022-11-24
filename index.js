ba806a8f93bfbfa7582d6a5cff6478fd5e7c6d20
Gaurav Rai Bhatt <gaurav.rai.bhatt@gmail.com>
2022-11-24 16:21:05 +0530

    updating CORS for A-C-A-Headers"

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*",);
+  res.header("Access-Control-Allow-Headers", "*",);
   next();
 });
 const port = process.env.PORT || 3000;
